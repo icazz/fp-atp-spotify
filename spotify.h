24fd200972a494c9e92ae96cd429682eb70e9997
icazz <icha.zika.1@gmail.com>
2024-12-03 20:50:58 +0700

    revisi

--- spotify.h
+++ spotify.h
@@ -20,11 +20,8 @@ namespace spotify {
     void addPlaylist(Playlist *&playlist);
     void deletePlaylist(Playlist *&playlist);
     void selectPlaylist(spotify::Playlist *playlist, spotify::Playlist *&selectedPlaylist);
-<<<<<<< HEAD
     void listPlaylistSongs(Playlist* playlist, int sortOption);
-=======
     //jangan lupa prototypenya, semangat gusy
->>>>>>> e6ebb0ef157625f4179f63c16ef3519b500a1b02
     
 }
 
