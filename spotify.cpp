914fb930cf410035ec18a25926c5997e96074383
shenaavv <vaneshakath@gmail.com>
2024-12-03 21:31:26 +0700

    menambahkan sorting option berdasarkan artis dan judul

--- spotify.cpp
+++ spotify.cpp
@@ -104,6 +104,55 @@ void spotify::selectPlaylist(spotify::Playlist *head, spotify::Playlist *&select
 // FUNGSI UNTUK MENU DETAIL PLAYLIST
 
 void listPlaylistSongs(Playlist* playlist, int sortOption){
+    if (!playlist || !playlist->head) {
+        std::cout << "No songs available in the playlist.\n";
+        return;
+    }
+
+    Song* currentSong = playlist->head;
+    Song* index = nullptr;
+    Song temp;
+
+    // Sorting berdasarkan pilihan 
+    switch (sortOption) {
+        case 1: // Sort by artis
+            for (currentSong = playlist->head; currentSong != nullptr; currentSong = currentSong->next) {
+                for (index = currentSong->next; index != nullptr; index = index->next) {
+                    if (currentSong->artist > index->artist) {
+                        // Tukar posisi lagu berdasarkan artist
+                        temp = *currentSong;
+                        *currentSong = *index;
+                        *index = temp;
+                    }
+                }
+            }
+            std::cout << "Songs sorted by artist.\n";
+            break;
+
+        case 2: // Sort by judul
+            for (currentSong = playlist->head; currentSong != nullptr; currentSong = currentSong->next) {
+                for (index = currentSong->next; index != nullptr; index = index->next) {
+                    if (currentSong->title > index->title) {
+                        // Tukar posisi lagu berdasarkan judul
+                        temp = *currentSong;
+                        *currentSong = *index;
+                        *index = temp;
+                    }
+                }
+            }
+            std::cout << "Songs sorted by title.\n";
+            break;
+
+        default: // Default sorting
+            std::cout << "Invalid sorting option. Showing default order.\n";
+            break;
+    }
+
+    // Menampilkan lagu setelah sorting
+    std::cout << "Songs in playlist '" << playlist->name << "':\n";
+    for (Song* song = playlist->head; song != nullptr; song = song->next) {
+        std::cout << "Title: " << song->title << ", Artist: " << song->artist << "\n";
+    }
     // isi sini
     // jangan lupa cek .h udah kutambahin prototype, nah kek gitu kalo mau buat fungsi baru
     // kalo program ga jalan wajar aja, soalnya menu ini nyambung sama menu playlist, belum ku benerin errornya hehe
